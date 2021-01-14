e764b6cf58e35b0903fd8b87514328190c88719b
Florian75011 <77019144+Florian75011@users.noreply.github.com>
2021-01-14 17:30:11 +0100

    Add files via upload

---
+++ lib/test.rb
@@ -0,0 +1,8 @@
+Array = ["John", "Mark"]
+
+def name_size_length
+    names = Array.new(20)
+    puts names.size  # This returns 20
+    puts names.length # This also returns 20
+end
+puts name_size_length
